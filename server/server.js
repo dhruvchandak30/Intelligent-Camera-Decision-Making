312dda103d3edea1396057593b740b93477091a5
harsh batra <harshbatra@harshs-MacBook-Air.local>
2024-01-14 15:23:24 +0530

    server created

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 require("dotenv").config({ path: __dirname + "/.env" });
+const connectDB = require('./utils/database');
 
 const app = express();
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send({
@@ -10,9 +15,7 @@ app.get("/", (req, res) => {
   });
 });
 
-
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server Listening on Port ${PORT}`);
 });
