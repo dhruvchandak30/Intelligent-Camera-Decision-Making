8e8dd591d867a82fce34c86a49af01a82d618b27
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-15 18:54:29 +0530

    Completed HomePage

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -7,38 +7,36 @@ const Hero = () => {
       className="text-white flex  flex-col lg:flex-row items-center justify-around
     "
     >
-      <div className="flex flex-col lg:w-1/4  rounded-3xl p-4 bg-gray-500 bg-opacity-60">
-        <div className="w-fit">
+      <div className="flex flex-col lg:w-1/4  rounded-3xl  bg-gray-500 bg-opacity-60">
+        <div className="">
           <img alt="Station" src={Station1} className=""></img>
         </div>
-        <div className="text-xl font-semibold p-8 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
-          asperiores fuga laboriosam, error accusantium doloremque, rerum
-          deserunt sunt illum officia cum explicabo voluptate. Consequuntur
-          alias, vel quas cum vitae ab. Cumque quis dolorem aspernatur quas
-          veniam ex. Esse harum rem eum sed modi quis recusandae pariatur. Dolor
-          voluptates fugit reiciendis.
+        <div className="text-xl px-4 font-semibold text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum id dicta
+          quibusdam tempore? Saepe, rerum voluptate laborum accusamus quaerat,
+          ex porro facere aspernatur fugit, necessitatibus perferendis tenetur
+          minima iure aliquam maxime accusantium tempore! Quidem repudiandae
+          sequi accusantium deserunt commodi! Alias.
         </div>
         <div className="flex justify-center">
-          <button className="flex items-center justify-center   px-20 py-3 m-4 font-bold text-2xl flex-shrink-0 rounded-2xl bg-gray-600 ">
+          <button className="flex items-center justify-center   px-20 py-3 m-4 font-bold text-2xl flex-shrink-0 rounded-2xl shadow-md  shadow-gray-900 bg-gray-700 hover:bg-gray-600 ">
             Continue
           </button>
         </div>
       </div>
-      <div className="flex flex-col lg:w-1/4 rounded-3xl p-4 bg-gray-500 bg-opacity-60">
-        <div className="w-fit">
+      <div className="flex flex-col lg:w-1/4 rounded-3xl  bg-gray-500 bg-opacity-60">
+        <div className="">
           <img alt="Station" src={Station2} className=""></img>
         </div>
-        <div className="text-xl font-semibold p-8 text-center">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum vel
-          molestiae vero, in assumenda ratione illum perspiciatis harum
-          cupiditate ipsam! Numquam minima fuga molestias odit alias dolore
-          porro ratione, commodi aut ex, pariatur quasi? Officiis laboriosam
-          modi tempore velit molestiae veniam quisquam ab, dolores ratione
-          doloremque obcaecati voluptatibus, vitae accusantium.
+        <div className="text-xl px-4 font-semibold  text-center">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci
+          temporibus, ipsum velit beatae doloremque fuga pariatur sed laudantium
+          exercitationem obcaecati sequi aliquam ex quis ratione magni culpa
+          animi quo odit modi inventore rem. Itaque libero, doloribus molestias
+          vitae voluptatum error.
         </div>
         <div className="flex justify-center">
-          <button className="flex items-center justify-center   px-20 py-3 m-4 font-bold text-2xl flex-shrink-0 rounded-2xl bg-gray-600">
+          <button className="flex items-center justify-center   px-20 py-3 m-4 font-bold text-2xl flex-shrink-0 rounded-2xl shadow-md  shadow-gray-900 bg-gray-700 hover:bg-gray-600">
             Continue
           </button>
         </div>
--- client/src/components/Home/Navbar.jsx
+++ client/src/components/Home/Navbar.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import logo from "../../assests/logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="text-white flex flex-col lg:flex-row items-center justify-around">
-      <div>
-        <img alt="logo" src={logo}></img>
+    <div className="text-white flex flex-col lg:flex-row items-center  mb-4">
+      <div className="flex items-center">
+        <Link to="/">
+          <img alt="logo" src={logo} className="mr-[35em]  py-3 h-16"></img>
+        </Link>
       </div>
-      <div>
+      <div className="flex items-center">
         <ul className="flex gap-20">
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Login</li>
+          <li className="cursor-pointer">About Us</li>
+          <li className="cursor-pointer">Contact Us</li>
+          <Link to="/login" className="cursor-pointer">
+            Login
+          </Link>
         </ul>
       </div>
     </div>
