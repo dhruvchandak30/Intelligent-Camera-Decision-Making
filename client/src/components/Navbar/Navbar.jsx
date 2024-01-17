bb6dac3ffb534fc7b93587fef22a11e647653307
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-17 12:03:53 +0530

    Added Language

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,126 @@
-import React from "react";
+import React, { Suspense } from "react";
 import logo from "../../assests/logo.svg";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 const Navbar = () => {
+  const { t, i18n } = useTranslation();
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  var userLang = navigator.language || navigator.userLanguage;
+  const [langMenu, setLangMenu] = useState(userLang);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  const changeLanguage = (lang) => {
+    if (lang === "Hindi") {
+      setLangMenu("HI");
+      i18n.changeLanguage("hi");
+    } else if (lang === "English") {
+      setLangMenu("EN");
+      i18n.changeLanguage("en");
+    } else if (lang === "Gujrati") {
+      i18n.changeLanguage("guj");
+    }
+    toggleDropdown();
+  };
   return (
-    <div className="relative">
-      <div className=" px-8 items-left">
-        <div className="py-6">
-          <Link to="/">
-            {" "}
-            <img src={logo} alt="" />
-          </Link>
+    <Suspense fallback="loading">
+      <div className="flex flex-row justify-between items-center">
+        <div className=" px-8 items-left">
+          <div className="py-6">
+            <Link to="/">
+              {" "}
+              <img src={logo} alt="" />
+            </Link>
+          </div>
+        </div>
+        <div className="">
+          <ul className="flex gap-x-8 text-white font-inter text-base font-normal">
+            <Link className="relative">
+              <li className="navbarli">{t("navbar1")}</li>
+            </Link>
+            <Link className="relative">
+              <li className="navbarli">{t("navbar2")}</li>
+            </Link>
+            <Link className="relative" to="/login">
+              <li className="navbarli">{t("navbar3")}</li>
+            </Link>
+          </ul>
+        </div>
+        {/* {t("")} */}
+        <div className="text-white flex flex-col m-4 ">
+          {/* <h1>{t("Language")}</h1> */}
+          <div className="relative inline-block text-left">
+            <button
+              type="button"
+              onClick={toggleDropdown}
+              className="inline-flex items-center justify-center w-max rounded-md border border-gray-300 px-3 py-2 bg-gray-800 text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+            >
+              Lang
+              <svg
+                className="-mr-1 ml-2 h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.293 7.293a1 1 0 0 1 1.414 0L10 11.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4a1 1 0 0 1-1.414 0l-4-4a1 1 0 0 1 0-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+
+            {isDropdownOpen && (
+              <div className="origin-top-right absolute right-0 mt-2 w-max rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                <div
+                  className="py-1"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="options-menu"
+                >
+                  <label className="block px-4 py-2 text-sm  text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                    <input
+                      type="radio"
+                      value="English"
+                      onClick={() => changeLanguage("English")}
+                      name="language"
+                      className="mr-2"
+                    />
+                    English - English
+                  </label>
+                  <label className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                    <input
+                      type="radio"
+                      value="Hindi"
+                      onClick={() => changeLanguage("Hindi")}
+                      name="language"
+                      className="mr-2"
+                    />
+                    हिंदी - Hindi
+                  </label>
+                  <label className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900">
+                    <input
+                      type="radio"
+                      value="Gujrati"
+                      onClick={() => changeLanguage("Gujrati")}
+                      name="language"
+                      className="mr-2"
+                    />
+                    ગુજરાતી - Gujrati
+                  </label>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
       </div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <ul className="flex gap-x-8 text-white font-inter text-base font-normal">
-          <Link className="relative">
-            <li className="navbarli">About Us</li>
-          </Link>
-          <Link className="relative">
-            <li className="navbarli">Contact Us</li>
-          </Link>
-          <Link className="relative" to="/login">
-            <li className="navbarli">Login</li>
-          </Link>
-        </ul>
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
